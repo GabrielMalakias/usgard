6a86654895fc0977674b3c29e6002cc36b9231ba
Gabriel Souza <gabriel.souza@locaweb.com.br>
2017-03-03 19:40:04 -0300

    Some changes

--- apps/web/assets/javascripts/sensors.js
+++ apps/web/assets/javascripts/sensors.js
@@ -1,29 +1,49 @@
-client = new Paho.MQTT.Client('localhost', Number('9001'), "usgard_" + parseInt(Math.random() * 100, 10));
+ // client = new Paho.MQTT.Client('localhost', Number('9001'), "usgard_" + parseInt(Math.random() * 100, 10));
+ //
+ // client.onConnectionLost = onConnectionLost;
+ // client.onMessageArrived = onMessageArrived;
+ //
+ // // connect the client
+ // client.connect({onSuccess:onConnect});
+ //
+ // // called when the client connects
+ // function onConnect() {
+ //   // Once a connection has been made, make a subscription and send a message.
+ //   console.log("onConnect");
+ //   client.subscribe("test");
+ //   message = new Paho.MQTT.Message("Hello");
+ //   message.destinationName = "World";
+ //   client.send(message);
+ // }
+ //
+ // // called when the client loses its connection
+ // function onConnectionLost(responseObject) {
+ //   if (responseObject.errorCode !== 0) {
+ //     console.log("onConnectionLost:"+responseObject.errorMessage);
+ //   }
+ // }
+ //
+ // // called when a message arrives
+ // function onMessageArrived(message) {
+ //   console.log("onMessageArrived:"+message.payloadString);
+ // }
+ //
 
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+var sensor = {
+  init: function (topic) {
+    this.client.onConnectionLost = this.onConnectionLost;
+    this.client.onMessageArrived = this.onMessageArrived;
 
-// connect the client
-client.connect({onSuccess:onConnect});
+    this.client.subscribe(topic);
+  },
+  client: new Paho.MQTT.Client('localhost', Number('9001'), "usgard_" + parseInt(Math.random() * 100, 10)),
 
-// called when the client connects
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("test");
-  message = new Paho.MQTT.Message("Hello");
-  message.destinationName = "World";
-  client.send(message);
-}
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
+  onConnectionLost: function (response) {
+    console.log("onConnectionLost: " + response.errorMessage);
+  },
+  onMessageArrived: function (message) {
+    console.log("onMessageArrived: " + message.payloadString);
   }
-}
+};
 
-// called when a message arrives
-function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
-}
+
---
+++ apps/web/views/sensors/show.rb
@@ -0,0 +1,6 @@
+module Web::Views::Sensors
+  class Show
+    include Web::View
+
+  end
+end
