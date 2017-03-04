2ba0b8f17c1749933f7f51a99df5db20e2d58f48
Gabriel Malaquias <gabriel07malakias@gmail.com>
2017-03-03 23:38:38 -0300

    Adds a draft to show statuses

--- apps/web/assets/javascripts/sensors.js
+++ apps/web/assets/javascripts/sensors.js
@@ -1,49 +1,35 @@
- // client = new Paho.MQTT.Client('localhost', Number('9001'), "usgard_" + parseInt(Math.random() * 100, 10));
- //
- // client.onConnectionLost = onConnectionLost;
- // client.onMessageArrived = onMessageArrived;
- //
- // // connect the client
- // client.connect({onSuccess:onConnect});
- //
- // // called when the client connects
- // function onConnect() {
- //   // Once a connection has been made, make a subscription and send a message.
- //   console.log("onConnect");
- //   client.subscribe("test");
- //   message = new Paho.MQTT.Message("Hello");
- //   message.destinationName = "World";
- //   client.send(message);
- // }
- //
- // // called when the client loses its connection
- // function onConnectionLost(responseObject) {
- //   if (responseObject.errorCode !== 0) {
- //     console.log("onConnectionLost:"+responseObject.errorMessage);
- //   }
- // }
- //
- // // called when a message arrives
- // function onMessageArrived(message) {
- //   console.log("onMessageArrived:"+message.payloadString);
- // }
- //
-
 var sensor = {
-  init: function (topic) {
-    this.client.onConnectionLost = this.onConnectionLost;
-    this.client.onMessageArrived = this.onMessageArrived;
-
-    this.client.subscribe(topic);
+  config: {
+    topic: '#mqtt_topic',
+    display_box: '#display_box',
+    mqttClient: null
   },
-  client: new Paho.MQTT.Client('localhost', Number('9001'), "usgard_" + parseInt(Math.random() * 100, 10)),
+  init: function () {
 
+    sensor.client().connect({onSuccess: sensor.onConnect});
+
+    sensor.client().onConnectionLost = sensor.onConnectionLost;
+    sensor.client().onMessageArrived = sensor.onMessageArrived;
+  },
+  onConnect: function () {
+    console.log("Connected");
+
+    var topic = $(sensor.config.topic).text();
+    sensor.client().subscribe(topic);
+    console.log("Subscribed " + topic);
+  },
   onConnectionLost: function (response) {
     console.log("onConnectionLost: " + response.errorMessage);
   },
   onMessageArrived: function (message) {
-    console.log("onMessageArrived: " + message.payloadString);
+    $(sensor.config.display_box).prepend("<p>" + message.payloadString + new Date().toLocaleTimeString() + '</p>');
+  },
+  client: function () {
+    if (sensor.mqttClient == null) {
+      sensor.mqttClient = new Paho.MQTT.Client('localhost', Number('9001'), "usgard_" + parseInt(Math.random() * 100, 10));
+    }
+    return sensor.mqttClient;
   }
 };
 
-
+$( document ).ready( sensor.init );
